2c7eeb75cf4b71c5c330663f6c731e44639ea698
Harshit <harshit.p1@learner.manipal.edu>
2024-11-15 14:21:13 +0530

    Initial Commit

---
+++ wecareTests/wecareTests.swift
@@ -0,0 +1,17 @@
+//
+//  wecareTests.swift
+//  wecareTests
+//
+//  Created by Harshit Pesala on 15/11/24.
+//
+
+import Testing
+@testable import wecare
+
+struct wecareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
